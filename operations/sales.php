480a1f29e3acb5588c4563174e99abb3dcd2316f
Odai Najjar <odai@customercareteam.com.au>
2012-04-20 17:48:14 +1000

    Issue #36

--- operations/sales.php
+++ operations/sales.php
@@ -31,6 +31,15 @@ div#users-contain table { margin: 1em 0; border-collapse: collapse; }
 div#users-contain table td, div#users-contain table th { border: 1px solid #eee; padding: .6em 10px; text-align: left; }
 </style>
 <script>
+window.onload=function()
+{
+	if ( $( "#centre_link" ).val() != "" )
+	{
+		$( "#centre" ).val($( "#centre_link" ).val());
+	}
+}
+</script>
+<script>
 $(function() {
 	$( "#datepicker" ).datepicker( {
 		showOn: "button",
@@ -41,7 +50,13 @@ $(function() {
 		altFormat: "dd/mm/yy",
 		changeMonth: true,
 		changeYear: true,
-		maxDate: "0d" });
+		maxDate: "0d",
+		onSelect: function(dateText, inst) {
+			var centre = $( "#centre" );
+		
+			$( "#details" ).load('sales_display.php?method=sales&centre=' + centre.val() + '&date=' + dateText);
+		}
+	});
 });
 </script>
 <script>
@@ -51,8 +66,6 @@ function Display()
 		date = $( "#datepicker" );
 		
 	$( "#details" ).load('sales_display.php?method=sales&centre=' + centre.val() + '&date=' + date.val());
-	
-	$( "#results" ).removeAttr('style');
 }
 </script>
 </head>
@@ -70,10 +83,11 @@ include "../source/operations_menu.php";
 
 <div id="text">
 
-<table style="margin-top:5px;">
+<input type="hidden" id="centre_link" value="<?php echo $_GET["centre"]; ?>" />
+<table width="100%">
 <tr>
-<td>Centre </td>
-<td width="80px"><select id="centre" style="height:auto; margin:0; padding:0; width:70px;">
+<td align="left"><img src="../images/sale_details_header2.png" width="125" height="25" style="margin-left:3px;" /></td>
+<td align="right" style="padding-right:10px;"><select id="centre" onchange="Display()" style="height:auto; margin:0; padding:0; width:70px;">
 <option></option>
 <?php
 $q = mysql_query("SELECT centres FROM operations WHERE user = '$ac[user]'") or die(mysql_error());
@@ -85,30 +99,34 @@ for ($i = 0; $i < count($centres); $i++)
 }
 ?>
 </select>
-</td>
-<td>Date</td>
-<td width="120px"><input type='text' size='11' id='datepicker2' style="height:20px;" readonly='readonly' value='<?php echo date("d/m/Y"); ?>' /><input type='hidden' id='datepicker' value='<?php echo date("Y-m-d"); ?>' /></td>
-<td><input type="button" onclick="Display()" class="search" value="" /></td>
+<input type='text' size='11' id='datepicker2' style="height:20px;" readonly='readonly' value='<?php echo date("d/m/Y"); ?>' /><input type='hidden' id='datepicker' value='<?php echo date("Y-m-d"); ?>' /></td>
+</tr>
+<tr>
+<td colspan="2"><img src="../images/line.png" width="100%" height="9" /></td>
 </tr>
 </table>
 
-<div id="results" style="display:none;">
-<p><img src="../images/sale_details_header2.png" width="125" height="25" style="margin-left:3px;" /></p>
-<p><img src="../images/line.png" width="740" height="9" /></p>
-<div id="users-contain" class="ui-widget">
-<table id="users" class="ui-widget ui-widget-content" width="100%">
+<center><div id="users-contain" class="ui-widget">
+<table id="users" class="ui-widget ui-widget-content" width="98%" style="margin-top:0px;">
 <thead>
 <tr class="ui-widget-header ">
 <th>Sale ID</th>
-<th>Creation Timestamp</th>
-<th>Sale Timestamp</th>
+<th>Final Status</th>
+<th>Agent</th>
+<th>Sale Type</th>
+<th>Last Updated</th>
 </tr>
 </thead>
 <tbody id="details">
+<script>
+var centre = $( "#centre_link" ),
+	date = $( "#datepicker" );
+		
+$( "#details" ).load('sales_display.php?method=sales&centre=' + centre.val() + '&date=' + date.val());
+</script>
 </tbody>
 </table>
-</div>
-</div>
+</div></center>
 
 </div>
 
--- operations/sales_display.php
+++ operations/sales_display.php
@@ -9,13 +9,15 @@ if ($method == "sales")
 	$centre = $_GET["centre"];
 	$date = $_GET["date"];
 	
-	$q = mysql_query("SELECT * FROM sales_customers WHERE status = 'Approved' AND centre = '$centre' AND DATE(approved_timestamp) = '$date'") or die(mysql_error());
+	$q = mysql_query("SELECT * FROM sales_customers WHERE status != 'Queue' AND centre = '$centre' AND DATE(approved_timestamp) = '$date' ORDER BY approved_timestamp ASC") or die(mysql_error());
 	
 	while ($data = mysql_fetch_assoc($q))
 	{
 		echo "<tr>";
 		echo "<td>" . $data["id"] . "</td>";
-		echo "<td>" . date("l, d/m/Y H:i A", strtotime($data["timestamp"])) . "</td>";
+		echo "<td>" . $data["status"] . "</td>";
+		echo "<td>" . $data["agent"] . "</td>";
+		echo "<td>" . $data["type"] . "</td>";
 		echo "<td>" . date("l, d/m/Y H:i A", strtotime($data["approved_timestamp"])) . "</td>";
 		echo "</tr>";
 	}
