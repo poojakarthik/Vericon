da7b2209ed36302761a7d560be989127aba02695
Odai Najjar <odai@customercareteam.com.au>
2012-04-24 14:29:12 +1000

    Issue #45

--- self/admin_details.php
+++ self/admin_details.php
@@ -157,10 +157,49 @@ if ($_GET["id"] == "")
 </form>
     <center><p class="error" style="color:#C00;"></p></center>
 </div>
-<p><img src="../images/sales_export_header.png" width="135" height="25" /></p>
-<p><img src="../images/line.png" width="740" height="9" /></p><br />
 
-<a style="color:#666;" href="sales_export.php?user=<?php echo $ac["user"] ?>" target="_blank"><?php echo $ac["centre"] . "_Sales_" . date("d_m_Y") . ".xls" ?></a>
+<p><img src="../images/submitted_sales_header.png" width="165" height="25" style="margin-left:3px;" /></p>
+<p><img src="../images/line.png" width="715" height="9" /></p>
+<div id="users-contain" class="ui-widget" style="width:100%; margin-left:3px; margin-top:-5px;">
+<table id="users" class="ui-widget ui-widget-content" width="95%">
+<thead>
+<tr class="ui-widget-header ">
+<th>Sale ID</th>
+<th>Lead ID</th>
+<th>Agent</th>
+<th>Campaign</th>
+<th>Type</th>
+</tr>
+</thead>
+<tbody>
+<?php
+$today = date("Y-m-d");
+$sq = mysql_query("SELECT * FROM sales_customers WHERE centre = '$ac[centre]' AND DATE(approved_timestamp) = '$today' ORDER BY approved_timestamp ASC") or die(mysql_error());
+if (mysql_num_rows($sq) == 0)
+{
+	echo "<tr>";
+	echo "<td colspan='5'><center>No Sales Submitted Today!</center></td>";
+	echo "</tr>";
+}
+else
+{
+	while ($sales_data = mysql_fetch_assoc($sq))
+	{
+		$aq = mysql_query("SELECT first,last FROM auth WHERE user = '$sales_data[agent]'") or die(mysql_error());
+		$agent = mysql_fetch_assoc($aq);
+		echo "<tr>";
+		echo "<td>" . $sales_data["id"] . "</td>";
+		echo "<td>" . $sales_data["lead_id"] . "</td>";
+		echo "<td>" . $agent["first"] . " " . $agent["last"] . "</td>";
+		echo "<td>" . $sales_data["campaign"] . "</td>";
+		echo "<td>" . $sales_data["type"] . "</td>";
+		echo "</tr>";
+	}
+}
+?>
+</tbody>
+</table>
+</div>
 <?php
 }
 else
