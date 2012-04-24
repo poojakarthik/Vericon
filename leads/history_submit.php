caa782d32dcb374e6c96b93fc1b3eac9283a7895
Odai Najjar <odai@customercareteam.com.au>
2012-04-24 17:08:36 +1000

    Issue #41

--- leads/history_submit.php
+++ leads/history_submit.php
@@ -9,18 +9,48 @@ if (preg_match('/0([2378])([0-9]{8})/',$id))
 	$id = substr($id,1,9);
 	
 	$q = mysql_query("SELECT * FROM log_leads WHERE cli = '$id'") or die(mysql_error());
-	while ($data = mysql_fetch_row($q))
+	
+	if (mysql_num_rows($q) == 0)
 	{
-		
+		echo "<tr><td colspan='4'>Lead Not Found!</td></tr>";
+	}
+	else
+	{
+		while ($data = mysql_fetch_assoc($q))
+		{
+			echo "<tr>";
+			echo "<td>" . $data["centre"] . "</td>";
+			echo "<td>" . date("d/m/Y", strtotime($data["issue_date"])) . "</td>";
+			echo "<td>" . date("d/m/Y", strtotime($data["expiry_date"])) . "</td>";
+			echo "<td>" . date("d/m/Y", strtotime($data["packet_expiry"])) . "</td>";
+			echo "</tr>";
+		}
 	}
 }
 elseif (preg_match('/([2378])([0-9]{8})/',$id))
 {
 	$q = mysql_query("SELECT * FROM log_leads WHERE cli = '$id'") or die(mysql_error());
+	
+	if (mysql_num_rows($q) == 0)
+	{
+		echo "<tr><td colspan='4'>Lead Not Found!</td></tr>";
+	}
+	else
+	{
+		while ($data = mysql_fetch_assoc($q))
+		{
+			echo "<tr>";
+			echo "<td>" . $data["centre"] . "</td>";
+			echo "<td>" . date("d/m/Y", strtotime($data["issue_date"])) . "</td>";
+			echo "<td>" . date("d/m/Y", strtotime($data["expiry_date"])) . "</td>";
+			echo "<td>" . date("d/m/Y", strtotime($data["packet_expiry"])) . "</td>";
+			echo "</tr>";
+		}
+	}
 }
 else
 {
-	$result = "Invalid Lead ID";
+	echo "<tr><td colspan='4'>Invalid Lead ID!</td></tr>";
 }
 
 ?>
