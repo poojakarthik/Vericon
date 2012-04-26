0214f3ba6caeb46ca1d986d0c1551bd4a3372edf
Odai Najjar <odai@customercareteam.com.au>
2012-04-26 16:59:52 +1000

    Issue #54

--- operations/rejections.php
+++ operations/rejections.php
@@ -4,7 +4,7 @@ include "../auth/iprestrict.php";
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>VeriCon :: Operations :: Stats</title>
+<title>VeriCon :: Operations :: Rejections</title>
 <link rel="shortcut icon" href="../images/vericon.ico">
 <link rel="stylesheet" href="../css/inner.css" type="text/css"/>
 <?php
