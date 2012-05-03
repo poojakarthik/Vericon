72cbeeee9a1bb18a06cd095858ed15835d99a8fc
Odai Najjar <odai@customercareteam.com.au>
2012-05-03 12:03:38 +1000

    Issue #70

---
+++ accounts/index.php
@@ -0,0 +1,37 @@
+<?php
+include "../auth/iprestrict.php";
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>VeriCon :: Accounts</title>
+<link rel="shortcut icon" href="../images/vericon.ico">
+<link rel="stylesheet" href="../css/inner.css" type="text/css"/>
+<?php
+include "../source/jquery.php";
+?>
+</head>
+
+<body>
+<div style="display:none;">
+
+</div>
+<div id="main_wrapper">
+
+<?php
+include "../source/header.php";
+include "../source/accounts_menu.php";
+?>
+
+<div id="text">
+
+
+
+</div>
+
+</div> 
+<?php
+include "../source/footer.php";
+?>
+</body>
+</html>
