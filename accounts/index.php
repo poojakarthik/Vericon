904caf78b67589095fceafd1fe33c8446cb5b008
Odai Najjar <odai@customercareteam.com.au>
2012-05-03 12:21:01 +1000

    Issue #73

--- accounts/index.php
+++ accounts/index.php
@@ -10,6 +10,51 @@ include "../auth/iprestrict.php";
 <?php
 include "../source/jquery.php";
 ?>
+<style>
+div#users-contain table { margin: 1em 0; margin-bottom:0; border-collapse: collapse; }
+div#users-contain table th { border: 1px solid #eee; padding: .6em 10px; text-align: left; }
+div#users-contain table td { border: 1px solid #eee; padding: .1em 10px; text-align: left; }
+</style>
+<script>
+$(function() {
+	$( "#datepicker" ).datepicker( {
+		showOn: "button",
+		buttonImage: "../images/calendar.gif",
+		buttonImageOnly: true,
+		dateFormat: "yy-mm-dd",
+		altField: "#datepicker2",
+		altFormat: "dd/mm/yy",
+		changeMonth: true,
+		changeYear: true,
+		maxDate: "0d",
+		onSelect: function(dateText, inst) {
+			var user = "<?php echo $ac["user"] ?>",
+				date2 = $( "#datepicker3" );
+			
+			$( "#display" ).load('display.php?user=' + user + '&date1=' + dateText + '&date2=' + date2.val());
+		}});
+});
+</script>
+<script>
+$(function() {
+	$( "#datepicker3" ).datepicker( {
+		showOn: "button",
+		buttonImage: "../images/calendar.gif",
+		buttonImageOnly: true,
+		dateFormat: "yy-mm-dd",
+		altField: "#datepicker4",
+		altFormat: "dd/mm/yy",
+		changeMonth: true,
+		changeYear: true,
+		maxDate: "0d",
+		onSelect: function(dateText, inst) {
+			var user = "<?php echo $ac["user"] ?>",
+				date1 = $( "#datepicker" );
+			
+			$( "#display" ).load('display.php?user=' + user + '&date1=' + date1.val() + '&date2=' + dateText);
+		}});
+});
+</script>
 </head>
 
 <body>
@@ -25,7 +70,26 @@ include "../source/accounts_menu.php";
 
 <div id="text">
 
+<table width="100%">
+<tr>
+<td align="left"><img src="../images/sale_stats_header.png" width="110" height="25" /></td>
+<td align="right" style="padding-right:10px;">
+<input type='text' size='9' id='datepicker2' readonly='readonly' style="height:20px;" value='<?php echo date("d/m/Y"); ?>' /><input type='hidden' id='datepicker' value='<?php echo date("Y-m-d"); ?>' /> to <input type='text' size='9' id='datepicker4' readonly='readonly' style="height:20px;" value='<?php echo date("d/m/Y"); ?>' /><input type='hidden' id='datepicker3' value='<?php echo date("Y-m-d"); ?>' />
+</td>
+</tr>
+<tr>
+<td colspan="2"><img src="../images/line.png" width="100%" height="9" /></td>
+</tr>
+</table>
+<div id="display">
+<script>
+var user = "<?php echo $ac["user"] ?>",
+	date1 = $( "#datepicker" );
+	date2 = $( "#datepicker3" );
 
+$( "#display" ).load('display.php?user=' + user + '&date1=' + date1.val() + '&date2=' + date2.val());
+</script>
+</div>
 
 </div>
 
