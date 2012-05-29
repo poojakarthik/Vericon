8d5e0d193219cb5eb0bb1e8fafba46a04dc8e255
Odai Najjar <odai@customercareteam.com.au>
2012-05-29 13:38:24 +1000

    Issue #113 & #114 & #115

--- qa/admin_process_submit.php
+++ qa/admin_process_submit.php
@@ -7,12 +7,12 @@ $date2 = date("d.m.Y", strtotime($date));
 $timestamp = date("Y-m-d H:i:s");
 $year = date("Y", strtotime($date));
 $month = date("F", strtotime($date));
-$report_path = "/home/dsr/" . $year . "/" . $month . "/DSR_" . $date2 . "_Report.txt";
-$dsr_b_path = "/home/dsr/" . $year . "/" . $month . "/DSR_" . $date2 . "_Business.csv";
-$dsr_r_path = "/home/dsr/" . $year . "/" . $month . "/DSR_" . $date2 . "_Residential.csv";
-$qa_path = "/home/dsr/" . $year . "/" . $month . "/DSR_" . $date2 . "_QA.csv";
 $year_path = "/home/dsr/" . $year;
-$directory_path = "/home/dsr/" . $year . "/" . $month;
+$month_path = $year_path . "/" . $month;
+$directory_path = $month_path . "/" . $date2;
+$report_path = $directory_path . "/DSR_" . $date2 . "_Report.txt";
+$dsr_b_path = $directory_path . "/DSR_" . $date2 . "_Business.csv";
+$dsr_r_path = $directory_path . "/DSR_" . $date2 . "_Residential.csv";
 $tmp_path = "/home/dsr/lock/tmp.txt";
 
 //Create temp Report
@@ -31,25 +31,19 @@ if (file_exists($report_path))
 else
 {
 	//Check if Directory Exists
-	if (file_exists($directory_path))
+	if (file_exists($month_path))
 	{
-		fwrite($fr,"Directory Already Exists\n");
+		fwrite($fr,"Month Directory Already Exists\n");
 	}
 	else
 	{
 		//Create Directories
-		if (file_exists($year_path))
-		{
-			fwrite($fr,"Creating Directory\n");
-			mkdir($directory_path,0755);
-		}
-		else
-		{
-			fwrite($fr,"Creating Directory\n");
-			mkdir($year_path,0755);
-			mkdir($directory_path,0755);
-		}
+		fwrite($fr,"Creating Month Directory\n");
+		mkdir($month_path,0755);
 	}
+	
+	fwrite($fr,"Creating Directory\n");
+	mkdir($directory_path,0755);	
 	
 	//Create Business DSR
 	fwrite($fr,"Generating Business DSR\n");
@@ -133,51 +127,9 @@ else
 	$resi_rows = mysql_num_rows($q1);
 	fwrite($fr,"--Added $resi_rows Rows\n");
 	
-	//Create QA DSR
-	fwrite($fr,"Generating QA DSR\n");
-	
-	$fq = fopen($qa_path, 'w+');
-
-	$header = "Agent,Centre,Date of Sale,Whoisit,Telco Name,Rating,Industry,CLI 1,Plan 1,CLI 2,Plan 2,CLI 3,Plan 3,CLI 4,Plan 4,CLI 5,Plan 5,CLI 6,Plan 6,CLI 7,Plan 7,CLI 8,Plan 8,CLI 9,Plan 9,CLI 10,Plan 10,WMSN 1,Wplan 1,WMSN 2,Wplan 2,ACLI,APLAN,Bundle,Contract Months,Other Comment";
-		
-	$q1 = mysql_query("SELECT Agent,Centre,Date_of_Sale,Whoisit,Telco_Name,Rating,Industry,CLI_1,Plan_1,CLI_2,Plan_2,CLI_3,Plan_3,CLI_4,Plan_4,CLI_5,Plan_5,CLI_6,Plan_6,CLI_7,Plan_7,CLI_8,Plan_8,CLI_9,Plan_9,CLI_10,Plan_10,WMSN_1,Wplan_1,WMSN_2,Wplan_2,ACLI,APLAN,Bundle,Contract_Months,Other_Comment FROM tmp_dsr WHERE date_of_sale <= '$date'") or die(mysql_error());
-	$data = "";
-	
-	if (mysql_num_rows($q1) != 0)
-	{
-		while ($da = mysql_fetch_row($q1))
-		{
-			foreach ($da as $col_value)
-			{
-				if (substr($col_value,0,1) != "0" || $col_value == "")
-				{
-					$data .= '"' . $col_value . '",';
-				}
-				elseif (preg_match("/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/",$col_value))
-				{
-					$data .= '"' . date("d/m/Y",strtotime($col_value));
-				}
-				else
-				{
-					$data .= '="' . $col_value . '",';
-				}
-			}
-			$data = substr_replace($data,"",-1);
-			$data .= "\n";
-		}
-	}
-	fwrite($fq, $header);
-	fwrite($fq, "\n");
-	fwrite($fq, $data);
-	fclose($fq);
-	
-	$qa_rows = mysql_num_rows($q1);
-	fwrite($fr,"--Added $qa_rows Rows\n");
-	
 	//Remove all exported records from tmp_dsr
 	fwrite($fr,"Deleting Rows from Temporary DSR Table\n");
 	mysql_query("DELETE FROM tmp_dsr WHERE date_of_sale <= '$date'") or die(mysql_error());
-	fwrite($fr,"--Deleted $qa_rows Rows\n");
 	
 	//Complete Report
 	fwrite($fr,"Generating Report");
