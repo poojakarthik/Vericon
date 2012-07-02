da22ddec098c70584a7501718d8f2091f106adbc
Odai Najjar <odai@customercareteam.com.au>
2012-07-02 15:28:43 +1000

    Issue #148

--- qa/sales.php
+++ qa/sales.php
@@ -3,123 +3,69 @@ include "../auth/iprestrict.php";
 include "../source/header.php";
 ?>
 <style>
-.search
-{
-	background-image:url('../images/search_btn.png');
-	background-repeat:no-repeat;
-	height:30px;
-	width:102px;
-	border:none;
-	background-color:transparent;
-}
-
-.search:hover
-{
-	background-image:url('../images/search_btn_hover.png');
-	cursor:pointer;
-}
-
 div#users-contain table { margin: 1em 0; border-collapse: collapse; }
-div#users-contain table td, div#users-contain table th { border: 1px solid #eee; padding: .6em 10px; text-align: left; }
+div#users-contain table th { border: 1px solid #eee; padding: .6em 10px; text-align: left; }
+div#users-contain table td { border: 1px solid #eee; padding: .3em 5px; text-align: left; }
 </style>
 <script>
 $(function() {
-	$( "#datepicker" ).datepicker( {
-		showOn: "button",
-		buttonImage: "../images/calendar.gif",
-		buttonImageOnly: true,
-		dateFormat: "yy-mm-dd",
-		altField: "#datepicker2",
-		altFormat: "dd/mm/yy",
-		changeMonth: true,
-		changeYear: true,
-		maxDate: "0d" });
+	$( "#dialog:ui-dialog_view_switch" ).dialog( "destroy" );
+	
+	$( "#dialog-confirm_view_switch" ).dialog({
+		autoOpen: false,
+		resizable: false,
+		draggable: false,
+		width: 375,
+		height: 100,
+		modal: true,
+		show: "blind",
+		hide: "blind"
+	});
+});
+
+function View(type)
+{
+	var date = $( "#store_date" ),
+		centre = $( "#store_centre" );
+	
+	$( "#store_type" ).val(type);
+	$( "#dialog-confirm_view_switch" ).dialog( "close" );
+	$( "#display2" ).hide('blind', '' , 'slow', function() {
+		$( "#display2" ).load('sales_display2.php?date=' + date.val() + '&centre=' + centre.val() + '&type=' + type,
+		function() {
+			$( "#display2" ).show('blind', '' , 'slow');
+		});
+	});
+}
+
+function View_Switch(centre)
+{
+	$( "#store_centre" ).val(centre);
+	$( "#dialog-confirm_view_switch" ).dialog( "open" );
+}
+</script>
+
+<div id="dialog-confirm_view_switch" title="Sale Status Type">
+<table width="100%" height="55px">
+<tr height="100%">
+<td valign="middle" align="center"><button onclick="View('Pending')" class="btn">Pending</button></td>
+<td valign="middle" align="center"><button onclick="View('Approved')" class="btn">Approved</button></td>
+<td valign="middle" align="center"><button onclick="View('Rejected')" class="btn">Rejected</button></td>
+</tr>
+</table>
+</div>
+
+<input type="hidden" id="store_date" value="" />
+<input type="hidden" id="store_centre" value="" />
+<input type="hidden" id="store_type" value="" />
+
+<div id="display">
+<script>
+$( "#display" ).load('sales_display.php?date=<?php echo date("Y-m-d"); ?>',
+function() {
+	$( "#display" ).show('blind', '' , 'slow');
 });
 </script>
-<script>
-function Display()
-{
-	var centre = $( "#centre" ),
-		date = $( "#datepicker" );
-		
-	$( "#stats" ).load('sales_submit.php?method=stats&centre=' + centre.val() + '&date=' + date.val());
-	$( "#details" ).load('sales_submit.php?method=pending&centre=' + centre.val() + '&date=' + date.val());
-	
-	$( "#results" ).removeAttr('style');
-}
-
-function Display_Type()
-{
-	var centre = $( "#centre" ),
-		display_type = $( "#display_type" ),
-		date = $( "#datepicker" );
-	$( "#details" ).load('sales_submit.php?method=' + display_type.val() + '&centre=' + centre.val() + '&date=' + date.val());
-}
-</script>
-
-<p><img src="../images/centre_sales_header.png" width="130" height="25" style="margin-left:3px;" /></p>
-<p><img src="../images/line.png" width="740" height="9" /></p>
-
-<table style="margin-top:5px;">
-<tr>
-<td>Centre </td>
-<td width="80px"><select id="centre" style="height:auto; margin:0; padding:0; width:70px;">
-<option></option>
-<?php
-$q = mysql_query("SELECT * FROM centres WHERE status = 'Active' ORDER BY centre ASC") or die(mysql_error());
-while ($centres = mysql_fetch_row($q))
-{
-	echo "<option>" . $centres[0] . "</option>";
-}
-?>
-</select>
-</td>
-<td>Date</td>
-<td width="120px"><input type='text' size='11' id='datepicker2' readonly='readonly' value='<?php echo date("d/m/Y"); ?>' /><input type='hidden' id='datepicker' value='<?php echo date("Y-m-d"); ?>' /></td>
-<td><input type="button" onclick="Display()" class="search" value="" /></td>
-</tr>
-</table>
-
-<div id="results" style="display:none;">
-<center>
-<div id="users-contain" class="ui-widget">
-<table id="users" class="ui-widget ui-widget-content" width="80%">
-<thead>
-<tr class="ui-widget-header ">
-<th>Campaign</th>
-<th style="text-align:center;">Total Sales</th>
-<th style="text-align:center;">Reworks</th>
-<th style="text-align:center;">Pending</th>
-<th style="text-align:center;">Approved</th>
-<th style="text-align:center;">Rejected</th>
-</tr>
-</thead>
-<tbody id="stats">
-</tbody>
-</table>
-</div>
-</center>
-<p><img src="../images/sale_details_header2.png" width="125" height="25" style="margin-left:3px;" />
-<select id="display_type" onchange="Display_Type()" style="height:auto; margin:0; padding:0; width:85px; float:right; margin-right:10px;">
-<option value="pending">Pending</option>
-<option value="rejected">Rejected</option>
-<option value="approved">Approved</option>
-</select></p>
-<p><img src="../images/line.png" width="740" height="9" /></p>
-<div id="users-contain" class="ui-widget">
-<table id="users" class="ui-widget ui-widget-content" width="100%">
-<thead>
-<tr class="ui-widget-header ">
-<th>Sale ID</th>
-<th>Lead ID</th>
-<th>Campaign</th>
-<th>Type</th>
-</tr>
-</thead>
-<tbody id="details">
-</tbody>
-</table>
-</div>
 </div>
 
 <?php
---
+++ qa/sales_display.php
@@ -0,0 +1,211 @@
+<?php
+mysql_connect('localhost','vericon','18450be');
+
+$date = $_GET["date"];
+
+$q0 = mysql_query("SELECT * FROM vericon.centres WHERE status = 'Active'") or die(mysql_error());
+$num = mysql_num_rows($q0);
+
+$t1 = ceil($num / 2);
+$t2 = $num - $t1;
+?>
+<script>
+$(function() {
+	$( "#datepicker" ).datepicker( {
+		showOn: "button",
+		buttonImage: "../images/calendar.png",
+		buttonImageOnly: true,
+		dateFormat: "yy-mm-dd",
+		altField: "#datepicker2",
+		altFormat: "dd/mm/yy",
+		firstDay: 1,
+		showOtherMonths: true,
+		selectOtherMonths: true,
+		changeMonth: true,
+		changeYear: true,
+		maxDate: "0d",
+		onSelect: function(dateText, inst) {
+			$( "#display" ).hide('blind', '' , 'slow', function() {
+				$( "#display" ).load('sales_display.php?date=' + dateText,
+				function() {
+					$( "#display" ).show('blind', '' , 'slow');
+				});
+			});
+			$( "#store_date" ).val(dateText);
+		}});
+});
+</script>
+
+<table width="100%">
+<tr>
+<td align="left" style="padding-left:5px;"><img src="../images/fresh_sales_header.png" width="120" height="25"></td>
+<td align="right" style="padding-right:10px;"><input type='text' size='11' id='datepicker2' readonly='readonly' value='<?php echo date("d/m/Y", strtotime($date)); ?>' /><input type='hidden' id='datepicker' value='<?php echo $date; ?>' /></td>
+</tr>
+<tr>
+<td colspan="2"><img src="../images/line.png" width="100%" height="9" /></td>
+</tr>
+</table>
+
+<table width="100%">
+<tr>
+<td width="50%" valign="top">
+<center><div id="users-contain" class="ui-widget">
+<table id="users" class="ui-widget ui-widget-content" width="98%" style="margin-top:0px;">
+<thead>
+<tr class="ui-widget-header ">
+<th style="text-align:center;" rowspan="2">Centre</th>
+<th style="text-align:center;" colspan="2">Pending</th>
+<th style="text-align:center;" colspan="2">Approved</th>
+<th style="text-align:center;" colspan="2">Rejected</th>
+<th style="text-align:center;" rowspan="2"></th>
+</tr>
+<tr class="ui-widget-header ">
+<th style="text-align:center;">B</th>
+<th style="text-align:center;">R</th>
+<th style="text-align:center;">B</th>
+<th style="text-align:center;">R</th>
+<th style="text-align:center;">B</th>
+<th style="text-align:center;">R</th>
+</tr>
+</thead>
+<tbody>
+<?php
+$q = mysql_query("SELECT centre FROM vericon.centres WHERE status = 'Active' ORDER BY centre ASC LIMIT 0,$t1") or die(mysql_error());
+while ($centre = mysql_fetch_row($q))
+{
+	$exclude_b = "";
+	$exclude_r = "";
+	
+	$q1e = mysql_query("SELECT id FROM vericon.qa_customers WHERE centre = '$centre[0]' AND type = 'Business' AND DATE(sale_timestamp) = '$date'") or die(mysql_error());
+	if (mysql_num_rows($q1e) != 0)
+	{
+		while ($ex = mysql_fetch_row($q1e))
+		{
+			$exclude_b .= " AND id != '$ex[0]' ";
+		}
+	}
+	$q1 = mysql_query("SELECT id FROM vericon.sales_customers WHERE status = 'Approved' AND centre = '$centre[0]' AND type = 'Business'" . $exclude_b . "AND DATE(approved_timestamp) = '$date'") or die(mysql_error());
+	$pending_b = mysql_num_rows($q1);
+	
+	$q2e = mysql_query("SELECT id FROM vericon.qa_customers WHERE centre = '$centre[0]' AND type = 'Residential' AND DATE(sale_timestamp) = '$date'") or die(mysql_error());
+	if (mysql_num_rows($q2e) != 0)
+	{
+		while ($ex = mysql_fetch_row($q2e))
+		{
+			$exclude_r .= " AND id != '$ex[0]' ";
+		}
+	}
+	$q2 = mysql_query("SELECT id FROM vericon.sales_customers WHERE status = 'Approved' AND centre = '$centre[0]' AND type = 'Residential'" . $exclude_r . "AND DATE(approved_timestamp) = '$date'") or die(mysql_error());
+	$pending_r = mysql_num_rows($q2);
+	
+	$q3 = mysql_query("SELECT id FROM vericon.qa_customers WHERE status = 'Approved' AND centre = '$centre[0]' AND type = 'Business' AND DATE(sale_timestamp) = '$date'") or die(mysql_error());
+	$approved_b = mysql_num_rows($q3);
+	
+	$q4 = mysql_query("SELECT id FROM vericon.qa_customers WHERE status = 'Approved' AND centre = '$centre[0]' AND type = 'Residential' AND DATE(sale_timestamp) = '$date'") or die(mysql_error());
+	$approved_r = mysql_num_rows($q4);
+	
+	$q5 = mysql_query("SELECT id FROM vericon.qa_customers WHERE status = 'Rejected' AND centre = '$centre[0]' AND type = 'Business' AND DATE(sale_timestamp) = '$date'") or die(mysql_error());
+	$rejected_b = mysql_num_rows($q5);
+	
+	$q6 = mysql_query("SELECT id FROM vericon.qa_customers WHERE status = 'Rejected' AND centre = '$centre[0]' AND type = 'Residential' AND DATE(sale_timestamp) = '$date'") or die(mysql_error());
+	$rejected_r = mysql_num_rows($q6);
+	
+	echo "<tr>";
+	echo "<td style='text-align:center'>" . $centre[0] . "</td>";
+	echo "<td style='text-align:center'>" . $pending_b . "</td>";
+	echo "<td style='text-align:center'>" . $pending_r . "</td>";
+	echo "<td style='text-align:center'>" . $approved_b . "</td>";
+	echo "<td style='text-align:center'>" . $approved_r . "</td>";
+	echo "<td style='text-align:center'>" . $rejected_b . "</td>";
+	echo "<td style='text-align:center'>" . $rejected_r . "</td>";
+	echo "<td style='text-align:center'><button onclick='View_Switch(\"$centre[0]\")' class='icon_view' title='View'></button></td>";
+	echo "</tr>";
+}
+?>
+</tbody>
+</table>
+</div></center>
+</td>
+<td width="50%" valign="top">
+<center><div id="users-contain" class="ui-widget">
+<table id="users" class="ui-widget ui-widget-content" width="98%" style="margin-top:0px;">
+<thead>
+<tr class="ui-widget-header ">
+<th style="text-align:center;" rowspan="2">Centre</th>
+<th style="text-align:center;" colspan="2">Pending</th>
+<th style="text-align:center;" colspan="2">Approved</th>
+<th style="text-align:center;" colspan="2">Rejected</th>
+<th style="text-align:center;" rowspan="2"></th>
+</tr>
+<tr class="ui-widget-header ">
+<th style="text-align:center;">B</th>
+<th style="text-align:center;">R</th>
+<th style="text-align:center;">B</th>
+<th style="text-align:center;">R</th>
+<th style="text-align:center;">B</th>
+<th style="text-align:center;">R</th>
+</tr>
+</thead>
+<tbody>
+<?php
+$q = mysql_query("SELECT centre FROM vericon.centres WHERE status = 'Active' ORDER BY centre ASC LIMIT $t1,$t2") or die(mysql_error());
+while ($centre = mysql_fetch_row($q))
+{
+	$exclude_b = "";
+	$exclude_r = "";
+	
+	$q1e = mysql_query("SELECT id FROM vericon.qa_customers WHERE centre = '$centre[0]' AND type = 'Business' AND DATE(sale_timestamp) = '$date'") or die(mysql_error());
+	if (mysql_num_rows($q1e) != 0)
+	{
+		while ($ex = mysql_fetch_row($q1e))
+		{
+			$exclude_b .= " AND id != '$ex[0]' ";
+		}
+	}
+	$q1 = mysql_query("SELECT id FROM vericon.sales_customers WHERE status = 'Approved' AND centre = '$centre[0]' AND type = 'Business'" . $exclude_b . "AND DATE(approved_timestamp) = '$date'") or die(mysql_error());
+	$pending_b = mysql_num_rows($q1);
+	
+	$q2e = mysql_query("SELECT id FROM vericon.qa_customers WHERE centre = '$centre[0]' AND type = 'Residential' AND DATE(sale_timestamp) = '$date'") or die(mysql_error());
+	if (mysql_num_rows($q2e) != 0)
+	{
+		while ($ex = mysql_fetch_row($q2e))
+		{
+			$exclude_r .= " AND id != '$ex[0]' ";
+		}
+	}
+	$q2 = mysql_query("SELECT id FROM vericon.sales_customers WHERE status = 'Approved' AND centre = '$centre[0]' AND type = 'Residential'" . $exclude_r . "AND DATE(approved_timestamp) = '$date'") or die(mysql_error());
+	$pending_r = mysql_num_rows($q2);
+	
+	$q3 = mysql_query("SELECT id FROM vericon.qa_customers WHERE status = 'Approved' AND centre = '$centre[0]' AND type = 'Business' AND DATE(sale_timestamp) = '$date'") or die(mysql_error());
+	$approved_b = mysql_num_rows($q3);
+	
+	$q4 = mysql_query("SELECT id FROM vericon.qa_customers WHERE status = 'Approved' AND centre = '$centre[0]' AND type = 'Residential' AND DATE(sale_timestamp) = '$date'") or die(mysql_error());
+	$approved_r = mysql_num_rows($q4);
+	
+	$q5 = mysql_query("SELECT id FROM vericon.qa_customers WHERE status = 'Rejected' AND centre = '$centre[0]' AND type = 'Business' AND DATE(sale_timestamp) = '$date'") or die(mysql_error());
+	$rejected_b = mysql_num_rows($q5);
+	
+	$q6 = mysql_query("SELECT id FROM vericon.qa_customers WHERE status = 'Rejected' AND centre = '$centre[0]' AND type = 'Residential' AND DATE(sale_timestamp) = '$date'") or die(mysql_error());
+	$rejected_r = mysql_num_rows($q6);
+	
+	echo "<tr>";
+	echo "<td style='text-align:center'>" . $centre[0] . "</td>";
+	echo "<td style='text-align:center'>" . $pending_b . "</td>";
+	echo "<td style='text-align:center'>" . $pending_r . "</td>";
+	echo "<td style='text-align:center'>" . $approved_b . "</td>";
+	echo "<td style='text-align:center'>" . $approved_r . "</td>";
+	echo "<td style='text-align:center'>" . $rejected_b . "</td>";
+	echo "<td style='text-align:center'>" . $rejected_r . "</td>";
+	echo "<td style='text-align:center'><button onclick='View_Switch(\"$centre[0]\")' class='icon_view' title='View'></button></td>";
+	echo "</tr>";
+}
+?>
+</tbody>
+</table>
+</div></center>
+</td>
+</tr>
+</table>
+
+<div id="display2">
+</div>
