87a894090fcc64b6eb533d11b72a83b83ce18e26
Odai Najjar <odai@customercareteam.com.au>
2012-07-02 17:49:12 +1000

    Issue #149

--- source/gnafGet.php
+++ source/gnafGet.php
@@ -546,7 +546,7 @@ elseif ($type == "display")
 		$state = $data["state"];
 		$postcode = $data["postcode"];
 		
-		echo $street_number . " " . $street_name . " " . $street_type . "}" . $suburb . "}" . $state . "}" . $postcode;
+		echo trim($street_number . " " . $street_name . " " . $street_type . "}" . $suburb . "}" . $state . "}" . $postcode);
 		
 	}
 	elseif (substr($id,0,2) == "MA")
@@ -575,7 +575,7 @@ elseif ($type == "display")
 			$street_number = $number_first;
 		}
 		
-		echo $building_type . " " . $building_number . " " . $building_name . " " . $street_number . " " . $street_name . " " . $street_type . "}" . $suburb . "}" . $state . "}" . $postcode;
+		echo trim($building_type . " " . $building_number . " " . $building_name . " " . $street_number . " " . $street_name . " " . $street_type . "}" . $suburb . "}" . $state . "}" . $postcode);
 	}
 }
 ?>
