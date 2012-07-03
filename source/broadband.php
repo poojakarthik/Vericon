a4c89c0acada6151b3b18ac72837a2d86dc144b6
Odai Najjar <odai@customercareteam.com.au>
2012-07-03 22:00:52 +1000

    Issue #163

--- source/broadband.php
+++ source/broadband.php
@@ -1,12 +1,11 @@
 <?php
-mysql_connect('localhost','adsl','18450be');
-mysql_select_db('adsl');
+mysql_connect('localhost','vericon','18450be');
 
 $phone = $_GET["phone"];
 
 if (preg_match('/0([2378])([0-9]{8})/',$phone,$d))
 {
-	$q = mysql_query("SELECT * FROM Enabled_Exchanges WHERE Range_From <= $d[2] AND Range_To >= $d[2] AND AC = '$d[1]'") or die(mysql_error());
+	$q = mysql_query("SELECT * FROM adsl.Enabled_Exchanges WHERE Range_From <= $d[2] AND Range_To >= $d[2] AND AC = '$d[1]'") or die(mysql_error());
 
 	if(mysql_num_rows($q) == 0)
 	{
@@ -15,7 +14,7 @@ if (preg_match('/0([2378])([0-9]{8})/',$phone,$d))
 	else
 	{
 		$r = mysql_fetch_row($q);
-		$t = mysql_num_rows(mysql_query("SELECT * FROM ADSL2P WHERE Exch_ID = '$r[2]'"));
+		$t = mysql_num_rows(mysql_query("SELECT * FROM adsl.ADSL2P WHERE Exch_ID = '$r[2]'"));
 		if($t > 0)
 		{
 			$net = "ADSL2+";
