fabb2599bf4583e86d254138ce10fa15b605a3dc
Odai Najjar <odai@customercareteam.com.au>
2012-07-09 18:16:43 +1000

    Issue #210

--- ma/index.php
+++ ma/index.php
@@ -3,7 +3,14 @@ include "../auth/iprestrict.php";
 include "../source/header.php";
 ?>
 
-
+<div id="display">
+<script>
+$( "#display" ).load('index_display.php',
+function() {
+	$( "#display" ).show('blind', '', 'slow');
+});
+</script>
+</div>
 
 <?php
 include "../source/footer.php";
--- source/header.php
+++ source/header.php
@@ -92,7 +92,7 @@ setInterval("Update_Clock()", 300000);
 </table>
 <table width="100%" height="24px" border="0" style="padding-right:23px;">
 <tr valign="bottom">
-<td align="right" valign="middle"><img src="../images/logout_icon.png" /> <a onclick="Logout()">Logout</a></td>
+<td align="right" valign="middle"><a href="/ma/">My Account</a>&nbsp;&nbsp;&nbsp;<img src="../images/logout_icon.png" /> <a onclick="Logout()">Logout</a></td>
 </tr>
 </table>
 </div>
