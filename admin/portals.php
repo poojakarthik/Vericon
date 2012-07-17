2e381fcbeec63e6d70d45a75698989faadcca56c
Odai Najjar <odai@customercareteam.com.au>
2012-07-17 13:17:47 +1000

    Issue #225

--- admin/portals.php
+++ admin/portals.php
@@ -3,8 +3,14 @@ include "../auth/iprestrict.php";
 include "../source/header.php";
 ?>
 
-<p><img src="../images/manage_portals_header.png" width="165" height="25" style="margin-left:5px;" /></p>
-<p><img src="../images/line.png" width="740" height="9" /></p>
+<div id="display">
+<script>
+$( "#display" ).load('portals_display.php',
+function() {
+	$( "#display" ).show('blind', '', 'slow');
+});
+</script>
+</div>
 
 <?php
 include "../source/footer.php";
