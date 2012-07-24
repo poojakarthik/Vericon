71d1de8d71f8feea0a91d67923ff4e1f3bbecb81
Odai Najjar <odai@customercareteam.com.au>
2012-07-24 14:36:10 +1000

    Issue #230

--- index.php
+++ index.php
@@ -76,6 +76,7 @@ if($_GET["attempt"] == "badip")
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>VeriCon :: Login</title>
 <link rel="shortcut icon" href="./images/vericon.ico">
 <link rel="stylesheet" href="css/style.css" type="text/css"/>
