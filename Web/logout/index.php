d85f41e849b6b0371cafe6d3b6ea3a8597574aa9
Odai Najjar <odai_najjar@hotmail.com>
2012-12-12 16:22:08 +1100

    Issue #358

--- Web/logout/index.php
+++ Web/logout/index.php
@@ -3,15 +3,15 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>VeriCon :: Logout</title>
-<link rel="shortcut icon" href="../images/vericon.ico">
-<link rel="stylesheet" href="../jquery/css/vc-theme/jquery-ui-1.9.2.custom.min.css">
-<script type="text/javascript" src="../jquery/js/jquery-1.8.3.js"></script>
-<script type="text/javascript" src="../jquery/js/jquery-ui-1.9.2.custom.min.js"></script>
-<link type="text/css" rel="stylesheet" href="../css/main.css" />
+<link rel="shortcut icon" href="/images/vericon.ico">
+<link type="text/css" rel="stylesheet" href="/css/main.css" />
 </head>
 
 <body>
-<h1>You have logged out successfully.</h1>
-<a href="/">Click Here</a> to login again.
+<center>
+<h1 style="margin-top:150px;">You have successfully logged out</h1><br><br>
+<img src="/images/logout.png"><br>
+<h3 style="text-decoration:underline;"><a href="/">Click Here to login again</a></h3>
+</center>
 </body>
 </html>
