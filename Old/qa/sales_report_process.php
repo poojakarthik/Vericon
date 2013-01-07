c90ca4205f20091edf1247860824918d72619fab
Odai Najjar <odai_najjar@hotmail.com>
2013-01-07 17:34:03 +1100

    Zone Telecom DSR Qucik Fix in Old

--- Old/qa/sales_report_process.php
+++ Old/qa/sales_report_process.php
@@ -8,5 +8,7 @@ if (!file_exists("/var/vtmp/dsr_loading.txt"))
 	exec("php /var/vericon/qa/zen_dsr.php Residential &> /dev/null");
 	exec("php /var/vericon/qa/nxt_dsr.php Business &> /dev/null");
 	exec("php /var/vericon/qa/nxt_dsr.php Residential &> /dev/null");
+	exec("php /var/vericon/qa/ztg_dsr.php Business &> /dev/null");
+	exec("php /var/vericon/qa/ztg_dsr.php Residential &> /dev/null");
 }
 ?>
