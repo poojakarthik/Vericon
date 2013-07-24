6ea2bc0d759fec4eb9c4029e2cfa9869c3f4d3dc
Odai Najjar <odai_najjar@hotmail.com>
2013-07-24 14:31:04 +1000

    Issue #454

--- Web/qa/sales_report_process.php
+++ Web/qa/sales_report_process.php
@@ -10,5 +10,7 @@ if (!file_exists("/var/vtmp/dsr_loading.txt"))
 	exec("php /var/vericon/qa/nxt_dsr.php Residential &> /dev/null");
 	exec("php /var/vericon/qa/ztg_dsr.php Business &> /dev/null");
 	exec("php /var/vericon/qa/ztg_dsr.php Residential &> /dev/null");
+	exec("php /var/vericon/qa/ltg_dsr.php Business &> /dev/null");
+	exec("php /var/vericon/qa/ltg_dsr.php Residential &> /dev/null");
 }
 ?>
